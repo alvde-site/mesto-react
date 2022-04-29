fdffac15092042b379e98bd7fff1d5623224530d
Aleksandr Demidenko <sashatro@mail.ru>
2022-04-29 10:16:53 +0300

    Добавить поля для формы через children

--- mesto-react/src/components/App.js
+++ mesto-react/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './header/Header';
 import Main from './main/Main';
 import Footer from './footer/Footer';
+import PopupWithForm from './popupWithForm/PopupWithForm';
 
 function App() {
   return (
@@ -10,42 +11,35 @@ function App() {
         <Header />
         <Main />
         <Footer />
+        <PopupWithForm name="profile" title="Редактировать профиль">
+          <label for="profilename" className="form__field">
+            <input id="profilename" type="text" className="form__input form__input_profile_name" name="profilename" placeholder="Введите имя" required minlength="2" maxlength="40" />
+            <span id="error-profilename" className="form__input-error"></span>
+          </label>
+          <label for="profilejob" className="form__field">
+            <input id="profilejob" type="text" className="form__input form__input_profile_job" name="profilejob" placeholder="Введите профессию" required minlength="2" maxlength="200" />
+            <span id="error-profilejob" className="form__input-error"></span>
+          </label>
+        </PopupWithForm>
+        <PopupWithForm name="add-element" title="Новое место">
+          <label for="addname" className="form__field">
+            <input id="addname" type="text" className="form__input form__input_add_name" name="name" placeholder="Название" required minlength="2" maxlength="30" />
+            <span id="error-addname" className="form__input-error"></span>
+          </label>
+          <label for="addlink" className="form__field">
+            <input id="addlink" type="url" className="form__input form__input_add_link" name="link" placeholder="Ссылка на картинку" required />
+            <span id="error-addlink" className="form__input-error"></span>
+          </label>
+        </PopupWithForm>
+        <PopupWithForm name="remove-confirm" title="Вы уверены?"></PopupWithForm>
+        <PopupWithForm name="edit-avatar" title="Обновить аватар">
+          <label for="addavatar" className="form__field">
+            <input id="addavatar" type="url" className="form__input form__input_add_link" name="link" placeholder="Ссылка на картинку" required />
+            <span id="error-addavatar" className="form__input-error"></span>
+          </label>
+        </PopupWithForm>
 
-
-        <div className="popup popup_handle_profile">
-          <div className="popup__container">
-            <button className="popup__close" type="button" aria-label="Закрыть"></button>
-            <form action="#" name="profileform" className="form" novalidate>
-              <h2 className="form__title">Редактировать профиль</h2>
-              <label for="profilename" className="form__field">
-                <input id="profilename" type="text" className="form__input form__input_profile_name" name="profilename" placeholder="Введите имя" required minlength="2" maxlength="40" />
-                  <span id="error-profilename" className="form__input-error"></span>
-              </label>
-              <label for="profilejob" className="form__field">
-                <input id="profilejob" type="text" className="form__input form__input_profile_job" name="profilejob" placeholder="Введите профессию" required minlength="2" maxlength="200" />
-                  <span id="error-profilejob" className="form__input-error"></span>
-              </label>
-              <button className="form__submit" type="submit">Сохранить</button>
-            </form>
-          </div>
-        </div>
-        <div className="popup popup_handle_add-element">
-          <div className="popup__container">
-            <button className="popup__close" type="button" aria-label="Закрыть"></button>
-            <form action="#" name="cardform" className="form" novalidate>
-              <h2 className="form__title">Новое место</h2>
-              <label for="addname" className="form__field">
-                <input id="addname" type="text" className="form__input form__input_add_name" name="name" placeholder="Название" required minlength="2" maxlength="30" />
-                  <span id="error-addname" className="form__input-error"></span>
-              </label>
-              <label for="addlink" className="form__field">
-                <input id="addlink" type="url" className="form__input form__input_add_link" name="link" placeholder="Ссылка на картинку" required />
-                  <span id="error-addlink" className="form__input-error"></span>
-              </label>
-              <button className="form__submit" type="submit">Сохранить</button>
-            </form>
-          </div>
-        </div>
+        {/*
         <div className="popup popup_handle_image-viewing">
           <div className="image-viewing">
             <button className="popup__close" type="button" aria-label="Закрыть"></button>
@@ -55,28 +49,7 @@ function App() {
             </figure>
           </div>
         </div>
-        <div className="popup popup_handle_remove-confirm">
-          <div className="popup__container">
-            <button className="popup__close" type="button" aria-label="Закрыть"></button>
-            <form action="#" name="confirmform" className="form" novalidate>
-              <h2 className="form__title">Вы уверены?</h2>
-              <button className="form__submit" type="submit">Да</button>
-            </form>
-          </div>
-        </div>
-        <div className="popup popup_handle_edit-avatar">
-          <div className="popup__container">
-            <button className="popup__close" type="button" aria-label="Закрыть"></button>
-            <form action="#" name="avatarform" className="form" novalidate>
-              <h2 className="form__title">Обновить аватар</h2>
-              <label for="addavatar" className="form__field">
-                <input id="addavatar" type="url" className="form__input form__input_add_link" name="link" placeholder="Ссылка на картинку" required />
-                  <span id="error-addavatar" className="form__input-error"></span>
-              </label>
-              <button className="form__submit" type="submit">Сохранить</button>
-            </form>
-          </div>
-  </div>
+        */}
       </div>
       {/*<template id="element_template">
         <li className="element">
@@ -90,7 +63,7 @@ function App() {
               </div>
             </div>
         </li>
-  </template>*/}
+      </template>*/}
     </>
   );
 }
---
+++ mesto-react/src/components/popupWithForm/PopupWithForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function PopupWithForm(props) {
+  return (
+    <>
+      <div className={`popup popup_handle_${props.name}`}>
+        <div className="popup__container">
+          <button className="popup__close" type="button" aria-label="Закрыть"></button>
+          <form action="#" name={`${props.name}form`} className="form" novalidate>
+            <h2 className="form__title">{props.title}</h2>
+            {props.children}
+            <button className="form__submit" type="submit">Сохранить</button>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default PopupWithForm;
