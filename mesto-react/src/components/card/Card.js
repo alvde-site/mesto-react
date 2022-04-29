eced93cee34e024af4ebb0bff95f42c64cc92f2d
Aleksandr Demidenko <sashatro@mail.ru>
2022-04-29 22:49:53 +0300

    Испрваить ошибки в ImagePopup.js

--- mesto-react/src/components/card/Card.js
+++ mesto-react/src/components/card/Card.js
@@ -7,19 +7,17 @@ function Card(props) {
   }
 
   return (
-    <>
-      <li className="element">
-        <button className="element__remove-button" type="button" aria-label="Удалить"></button>
-        <img src={props.card.link} alt={props.card.name} className="element__img" onClick={handleClick}/>
-        <div className="element__description">
-          <h2 className="element__description-text">{props.card.name}</h2>
-          <div className="element__like-block">
-            <button className="element__like-button" type="button" aria-label="Понравилось"></button>
-            <span className="element__like-count">{props.card.likes}</span>
-          </div>
+    <li className="element">
+      <button className="element__remove-button" type="button" aria-label="Удалить"></button>
+      <img src={props.card.link} alt={props.card.name} className="element__img" onClick={handleClick} />
+      <div className="element__description">
+        <h2 className="element__description-text">{props.card.name}</h2>
+        <div className="element__like-block">
+          <button className="element__like-button" type="button" aria-label="Понравилось"></button>
+          <span className="element__like-count">{props.card.likes}</span>
         </div>
-      </li>
-    </>
+      </div>
+    </li>
   )
 }
 
--- mesto-react/src/components/imagePopup/ImagePopup.js
+++ mesto-react/src/components/imagePopup/ImagePopup.js
@@ -2,17 +2,15 @@ import React from 'react';
 
 function ImagePopup(props) {
   return (
-    <>
-      <div className={`popup popup_handle_${props.name} ${props.card.isOpen ? 'popup_opened' : ''}`}>
-        <div className="image-viewing">
-          <button className="popup__close" type="button" aria-label="Закрыть" onClick={props.onClose}></button>
-          <figure className="image-viewing__img-card">
-            <img src={props.card.link} alt={props.card.name} className="image-viewing__image" />
-            <figcaption className="image-viewing__caption">{props.card.name}</figcaption>
-          </figure>
-        </div>
+    <div className={`popup popup_handle_${props.name} ${props.card.isOpen && 'popup_opened'}`}>
+      <div className="image-viewing">
+        <button className="popup__close" type="button" aria-label="Закрыть" onClick={props.onClose}></button>
+        <figure className="image-viewing__img-card">
+          <img src={props.card.link} alt={props.card.name} className="image-viewing__image" />
+          <figcaption className="image-viewing__caption">{props.card.name}</figcaption>
+        </figure>
       </div>
-    </>
+    </div>
   )
 }
 
