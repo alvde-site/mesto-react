e32a90ec1a282d1566037e79fa8abd1742fa7f5c
Aleksandr Demidenko <sashatro@mail.ru>
2022-05-12 14:13:01 +0300

    refactoring of changeLikeCardStatus

--- mesto-react/src/utils/Api.js
+++ mesto-react/src/utils/Api.js
@@ -75,19 +75,9 @@ class Api {
 
   changeLikeCardStatus(cardId, notLiked) {
     if (notLiked) {
-      return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
-        method: "PUT",
-        headers: {
-          authorization: this._authorization,
-        },
-      }).then(this._checkResponse);
+      return this.addLike(cardId)
     } else {
-      return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
-        method: "DELETE",
-        headers: {
-          authorization: this._authorization,
-        },
-      }).then(this._checkResponse);
+      return this.removeLike(cardId)
     }
   }
 
